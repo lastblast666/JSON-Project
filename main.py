f14f2adbf72d31529f404655a84796d9bfc2ce0d
lastblast666 <krasicki.alexandre@gmail.com>
2020-11-03 16:07:22 +0100

    updated json

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+from py2neo import Graph, Node, Relationship
+import os
 import pandas as pd
 
-data = pd.read_json('C:\Krasicki_Alexandre_Hitema\JSON Project\movies_rated_tagged.json')
+URL = os.environ.get("http://localhost:7474/")
+connect = Graph(URL)
+connect.delete_all()
 
-print(data)
+Chemin = 'C:/Krasicki_Alexandre_Hitema/JSON Project/movies_rated_tagged.json'
+data = pd.read_json(Chemin)
+
+data = data.drop_duplicates(subset=['title'])
+
+for film in data['title']:
+    print("Send {} into node".format(film))
+    node_Title = Node('Titre film', name=film)
+    connect.create(node_Title)
+
+unique_year_list = []
+for year in data['year']:
+    if year not in unique_year_list:
+        unique_year_list.append(year)
+        print("Send {} into node".format(year))
+        node_Title = Node('Année film', name=year)
+        connect.create(node_Title)
+
+
+#film_years = Relationship(film, "KNOWS", year)
+#graph.create(film_years)
